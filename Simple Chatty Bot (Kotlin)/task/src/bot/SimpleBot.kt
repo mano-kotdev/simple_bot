0e3218c46e681f9cc3dfc80c553f243b5f2b3151
Manoj <manoj.m@honeywell.com>
2023-03-27 10:50:58 +0530

    Initial commit

---
+++ Simple Chatty Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -0,0 +1,73 @@
+package bot
+
+import java.util.Scanner
+
+
+val scanner = Scanner(System.`in`)// Do not change this line
+val crtAnswer = 1
+var shouldShowQuiz = true
+
+fun main() {
+    greet("Aid", "2020") // change it as you need
+    remindName()
+    guessAge()
+    count()
+    test()
+    end()
+}
+
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
+    println("Please, remind me your name.")
+}
+
+fun remindName() {
+    val name = scanner.nextLine()
+    println("What a great name you have, ${name}!")
+}
+
+fun guessAge() {
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    println("Your age is ${age}; that's a good time to start programming!")
+}
+
+fun count() {
+    println("Now I will prove to you that I can count to any number you want.")
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        print(i)
+        println("!")
+    }
+}
+
+fun test() {
+    println("Let's test your programming knowledge.")
+    // write your code here
+    while(shouldShowQuiz){
+        showQuiz()
+    }
+}
+
+fun showQuiz() {
+    println("Why do we use methods?")
+    println("1. correct answer")
+    println("2. wrong answer")
+    println("3. wrong answer")
+    println("4. wrong answer")
+    val readNum = readln().toInt()
+    if(crtAnswer == readNum){
+        shouldShowQuiz = false
+    }else{
+        println("Please, try again.")
+    }
+}
+
+fun end() {
+    println("Congratulations, have a nice day!") // Do not change this text
+}
